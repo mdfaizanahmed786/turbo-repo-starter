133c2d0b80ec612908e045169b1ef9a1f222d4ca
Faizan Ahmed <faizanzaid789@gmail.com>
2024-07-13 20:07:05 +0530

    added another backend repo

---
+++ packages/ui/src/input-container.tsx
@@ -0,0 +1,8 @@
+export const InputContainer = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div>
+      <h1>InputContainer Component</h1>
+      {children}
+    </div>
+  );
+};
