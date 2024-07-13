497d85dfd003b7749b1641dffa1160feb2ed7ebf
Faizan Ahmed <faizanzaid789@gmail.com>
2024-07-13 21:56:57 +0530

    shared a common module to backend folder

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 const app=express();
-
+import{BASE_URL} from "@repo/common/config"
+console.log(BASE_URL);  
 app.get('/',(req,res)=>{
     res.send('Hello World');
 });
